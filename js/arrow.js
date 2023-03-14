edd049e67f62d2b1a7adf7ea01972bba3ddcdd40
Jack Kennedy <ps24jkennedy@efcts.us>
2023-03-14 10:44:56 -0400

    .

--- js/arrow.js
+++ js/arrow.js
@@ -2,5 +2,4 @@ const arrow = document.getElementById("selectable-arrow-bit");
 
 arrow.addEventListener("click", function() {
     document.getElementById("arrow").classList.remove("bounce");
-    window.scrollTo(0, window.innerHeight * 1.1)
 })
